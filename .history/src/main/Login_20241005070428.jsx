6de24e23a2244393282f66e7f0129692cb29a640
Danish <azkadanish2407@gmail.com>
2024-10-05 07:06:17 +0700

    eror handling 2

--- .history/src/main/Login_20241005070428.jsx
+++ .history/src/main/Login_20241005070428.jsx
@@ -30,7 +30,7 @@ const Login = () => {
       let user = users.find(user => input.value === user.email && input2.value === user.password);
 
       if (user) {
-        window.location.href = '/pts-rentCarKuu/display';
+        window.location.href = '/pts-rentCarKuu//display';
           localStorage.setItem('nama', user.nama);
           localStorage.setItem('email', user.email);
           localStorage.setItem('pp', user.photo);
