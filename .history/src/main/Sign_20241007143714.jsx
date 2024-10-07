9663038c868ae01fe1116a9aee06c538a12dcd9c
Danish <azkadanish2407@gmail.com>
2024-10-07 14:40:25 +0700

    handling 3

--- .history/src/main/Sign_20241007143714.jsx
+++ .history/src/main/Sign_20241007143714.jsx
@@ -80,7 +80,7 @@ function Sign() {
                         />
                     </div>
                     <button
-                        
+                    
                         type="submit"
                         className="w-full bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300"
                     >
